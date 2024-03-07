9553965a38ff9b43e9b51664f080e2148de402e1
Kembec <dev@kembec.com>
2024-03-06 19:26:22 -0500

    [added] prettier config

--- bin/cli.js
+++ bin/cli.js
@@ -5,7 +5,7 @@ import { program } from "commander";
 import { setupProject } from "../src/index.js";
 
 program
-	.version("1.0.1")
+	.version("1.0.2")
 	.description("Configure ESLint for Astro projects")
 	.option("-p, --path <path>", "Specify the project path. Defaults to ./", "./")
 	.action((options) => {
--- src/index.js
+++ src/index.js
@@ -23,6 +23,30 @@ export async function setupProject(options) {
 		},
 	]);
 
+	const prettierConfig = {
+		plugins: ["prettier-plugin-astro"],
+		arrowParens: "always",
+		bracketSameLine: false,
+		bracketSpacing: true,
+		semi: true,
+		experimentalTernaries: false,
+		singleQuote: false,
+		jsxSingleQuote: false,
+		quoteProps: "as-needed",
+		trailingComma: "es5",
+		singleAttributePerLine: true,
+		htmlWhitespaceSensitivity: "css",
+		vueIndentScriptAndStyle: true,
+		proseWrap: "preserve",
+		insertPragma: false,
+		printWidth: 120,
+		requirePragma: false,
+		tabWidth: 4,
+		useTabs: true,
+		embeddedLanguageFormatting: "auto",
+		endOfLine: "lf",
+	};
+
 	const eslintConfig = {
 		extends: ["@kembec/eslint-config/typescript", "plugin:astro/base"],
 		overrides: [
@@ -66,7 +90,8 @@ export async function setupProject(options) {
 	}
 
 	installDependencies(dependencies, projectPath, answers.packageManager);
-	await fs.writeJSON(path.join(projectPath, ".eslintrc.json"), eslintConfig, { spaces: 2 });
+	await fs.writeJSON(path.join(projectPath, ".eslintrc"), eslintConfig, { spaces: 2 });
+	await fs.writeJSON(path.join(projectPath, ".prettierrc"), prettierConfig, { spaces: 2 });
 
 	console.log(`Setup completed for ${answers.frameworks.join(", ")} at ${projectPath}`);
 }
